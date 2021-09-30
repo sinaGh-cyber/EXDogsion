e238f3b483f041c88c707136a601d3e8d7507fee
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-10-01 01:08:07 +0330

    background.js & defaultAction button added

---
+++ background.js
@@ -0,0 +1,11 @@
+const swapingOrder = "swap images with dogyes";
+const msg = {
+  txt: swapingOrder,
+};
+const defaultIconHandler = (tab) => {
+  console.log('I am runing');
+
+  chrome.tabs.sendMessage(tab.id, msg);
+};
+console.log('hello');
+chrome.browserAction.onClicked.addListener(defaultIconHandler);
--- content.js
+++ content.js
@@ -5,12 +5,22 @@ const fileNames = [
   'taylor-kopel-WX4i1Jq_o0Y-unsplash.jpg',
   'victor-grabarczyk-N04FIfHhv_k-unsplash.jpg',
 ];
-
 const imags = document.getElementsByTagName('img');
+const swapingOrder = 'swap images with dogyes';
 
-for (imag of imags) {
-  const fileNum = Math.floor(Math.random() * fileNames.length);
-  const file = 'dogs/' + fileNames[fileNum];
-  const url = chrome.extension.getURL(file);
-  imag.src = url;
-}
+const imagSwaper = () => {
+  console.log('imagSwaper running');
+  for (imag of imags) {
+    const fileNum = Math.floor(Math.random() * fileNames.length);
+    const file = 'dogs/' + fileNames[fileNum];
+    const url = chrome.extension.getURL(file);
+    imag.src = url;
+  }
+};
+
+const proccesMessagHandler = (messag, sender, sendResponse) => {
+  console.log('proccesMessagHandler running');
+  if (messag.txt === swapingOrder) imagSwaper();
+};
+
+chrome.runtime.onMessage.addListener(proccesMessagHandler);
